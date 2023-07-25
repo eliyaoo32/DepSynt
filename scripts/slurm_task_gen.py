7cab84164543e1476690f2762046671fdcb30596
Eliyahu basa <eliyaoo32@gmail.com>
2023-07-25 20:02:27 +0300

    Created depsynt task runner

--- scripts/slurm_task_gen.py
+++ scripts/slurm_task_gen.py
@@ -6,12 +6,55 @@ from os import path
 from pathlib import Path
 
 parser = argparse.ArgumentParser("Generate slurm tasks for finding dependency and synthesis")
-parser.add_argument("--task", type=str, required=True, choices=['find_deps'], help="Type of task to generate")
+parser.add_argument("--task", type=str, required=True, choices=['find_deps', 'depsynt'], help="Type of task to generate")
 parser.add_argument("--timeout", type=str, required=True, help="Timeout for each task, for example, 60m")
 parser.add_argument("--benchmarks-path", type=str, required=True, help="Path for the benchmarks in text file")
 parser.add_argument("--output-path", type=str, required=True, help="Path for put the output files")
+parser.add_argument("--families", type=str, required=True, help="List of benchmarks families to run, separated by comma, for example, mux,adder")
 
-if __name__ == '__main__':
+
+def generate_depsynt(args):
+    benchmarks_path = args.benchmarks_path
+    total_benchmarks = len([f for f in glob(path.join(benchmarks_path, "*.txt"))])
+    task_template = Path(path.join(Path(__file__).parent.resolve(), 'synthesis_slurm_template.sh')).read_text()
+    families = args.families.split(',')
+    if len(families) == 0:
+        print("Error: no families provided")
+        exit(1)
+
+    variables = {
+        'OUTPUT_BASE_PATH': args.output_path,
+        'NUM_BENCHMARKS': str(total_benchmarks),
+        'TIMEOUT': args.timeout,
+        'BENCHMARKS_DIR': benchmarks_path,
+        'ALLOWED_FAMILIES': " ".join(["\""+f+"\"" for f in families]),
+    }
+
+    for var_name, var_value in variables.items():
+        task_template = task_template.replace('{{'+var_name+'}}', var_value)
+
+    print(task_template)
+
+
+def generate_find_deps(args):
+    benchmarks_path = args.benchmarks_path
+    total_benchmarks = len([f for f in glob(path.join(benchmarks_path, "*.txt"))])
+    task_template = Path(path.join(Path(__file__).parent.resolve(), 'find_deps_slurm_template.sh')).read_text()
+
+    variables = {
+        'OUTPUT_BASE_PATH': args.output_path,
+        'NUM_BENCHMARKS': str(total_benchmarks),
+        'TIMEOUT': args.timeout,
+        'BENCHMARKS_DIR': benchmarks_path
+    }
+
+    for var_name, var_value in variables.items():
+        task_template = task_template.replace('{{'+var_name+'}}', var_value)
+
+    print(task_template)
+
+
+def main():
     args = parser.parse_args()
     benchmarks_path = args.benchmarks_path
 
@@ -25,19 +68,13 @@ if __name__ == '__main__':
         exit(1)
 
     if args.task == 'find_deps':
-        task_template = Path(path.join(Path(__file__).parent.resolve(), 'find_deps_slurm_template.sh')).read_text()
-
-        variables = {
-            'OUTPUT_BASE_PATH': args.output_path,
-            'NUM_BENCHMARKS': str(total_benchmarks),
-            'TIMEOUT': args.timeout,
-            'BENCHMARKS_DIR': benchmarks_path
-        }
-
-        for var_name, var_value in variables.items():
-            task_template = task_template.replace('{{'+var_name+'}}', var_value)
-
-        print(task_template)
+        generate_find_deps(args)
+    elif args.task == 'depsynt':
+        generate_depsynt(args)
     else:
         print("Unknown task")
         exit(1)
+
+
+if __name__ == '__main__':
+    main()
