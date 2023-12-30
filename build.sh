3664a8eca55753fd5d29e53ba6d6b35bf8283e59
Eliyaoo32 <your_email@example.com>
2023-12-30 12:17:57 +0200

    Add JSON headers to the submodules

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 # Install packages
 cd ./packages
 sudo dpkg -i *.deb
+# Create symbolic link of libboost_json.so
+sudo ln -s /usr/lib/x86_64-linux-gnu/libboost_json.so.1.83.0 /usr/lib/x86_64-linux-gnu/libboost_json.so
 cd ../
 
 # Build ABC
@@ -12,4 +14,5 @@ cd ../../
 # Build Synthesis
 cmake .
 make synthesis
-ln -s ./synthesis /usr/local/bin/synthesis
+ln -s ./synthesis /usr/local/bin/depsynt
+ln -s ./synthesis ./depsynt
